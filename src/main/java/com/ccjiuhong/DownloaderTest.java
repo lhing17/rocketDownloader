cca4717edf728aa9303430ce44d1fd92e29af075
lhing17 <lhing17@163.com>
2019-06-30 10:57:20 +0800

    升级为jdk11。

--- src/main/java/com/ccjiuhong/DownloaderTest.java
+++ src/main/java/com/ccjiuhong/DownloaderTest.java
@@ -19,7 +19,9 @@ public class DownloaderTest {
     public static void main(String[] args) {
         DownloadManager downloadManager = DownloadManager.getInstance();
         String fileUrl = "https://dldir1.qq.com/qqfile/qq/PCQQ9.1.5/25530/QQ9.1.5.25530.exe";
-        String targetFileName = fileUrl.substring(fileUrl.lastIndexOf("/"));
+//        String fileUrl = "https://download.oracle.com/otn/java/jdk/11.0.3+12/37f5e150db5247ab9333b11c1dddcd30/jdk-11.0.3_windows-x64_bin.zip?AuthParam=1561862894_d3c07f3538fb5d9f5c2a3df110fadbe4";
+        String targetFileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
+//        String targetFileName = "jdk-11.0.3_windows-x64_bin.zip";
         int missionId = downloadManager.addMission(fileUrl, "F:\\rocketDownloader", targetFileName);
         downloadManager.startMission(missionId);
 
